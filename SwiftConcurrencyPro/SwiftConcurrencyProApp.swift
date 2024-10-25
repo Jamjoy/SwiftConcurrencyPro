8ec87578f4ac8ff870a95780b7e1756b94b34626
Joyanta Mondal <joyantamondal156@gmail.com>
2024-10-25 20:21:03 +0530

    Initial Commit

---
+++ SwiftConcurrencyPro/SwiftConcurrencyProApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyProApp.swift
+//  SwiftConcurrencyPro
+//
+//  Created by Joyanta Mondal on 25/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
