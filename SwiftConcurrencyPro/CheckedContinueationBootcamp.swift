07def982fe42f250115aa396af8420250f370b5e
Joyanta Mondal <joyantamondal156@gmail.com>
2024-10-29 15:35:57 +0530

    taskgroup implementation

---
+++ SwiftConcurrencyPro/CheckedContinueationBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  CheckedContinueationBootcamp.swift
+//  SwiftConcurrencyPro
+//
+//  Created by Joyanta Mondal on 26/10/24.
+//
+
+import SwiftUI
+
+struct CheckedContinueationBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckedContinueationBootcamp()
+}
---
+++ SwiftConcurrencyPro/TaskBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  TaskBootcamp.swift
+//  SwiftConcurrencyPro
+//
+//  Created by Joyanta Mondal on 25/10/24.
+//
+
+import SwiftUI
+
+struct TaskBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskBootcamp()
+}
